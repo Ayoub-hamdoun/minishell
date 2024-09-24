c396ce7389ab7f73023c57f6b1c65032848c427b
ayoub <ayoub.hamdoun.g@gmail.com>
2024-09-24 11:54:33 +0100

    in the name of god nbdaw minishell; UPDATES : - working on a tokenizer - cleaner wrokspace (some folders added to the repo) GOODLUCK :)

--- minishell.h
+++ minishell.h
@@ -6,7 +6,50 @@
 /*   By: ayhamdou <ayhamdou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 15:40:38 by ayhamdou          #+#    #+#             */
-/*   Updated: 2024/09/05 15:40:39 by ayhamdou         ###   ########.fr       */
+/*   Updated: 2024/09/24 11:50:10 by ayhamdou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// utils
+char	**ft_split(char *s, char c);
+
+// parsing funcs
+
+typedef enum e_type
+{
+	WORD,
+	R_IN,
+	R_OUT,
+	APP,
+	HER,
+	PIP
+}	e_type;
+
+// Token structure
+typedef struct s_token
+{
+	char			str;
+	e_type			token;
+	struct s_token	*next;
+}	t_token;
+
+// Redirection structure
+typedef struct s_redir
+{
+	char			filename;
+	e_type			type; // Changed e_num to e_type
+	struct s_redir	*next; // Fix the next pointer to s_redir
+}	t_redir;
+
+// Command structure
+typedef struct s_command
+{
+	char				*args;
+	int					is_builtin;
+	struct s_redir		*rederects; // Fix the pointer to s_redir
+	struct s_command	*next; // Fix the next pointer to s_command
+}	t_command;
+
+
+void	parser(char *userInp);
+// end of parsing funcs
--- parsing/parser.c
+++ parsing/parser.c
@@ -6,7 +6,29 @@
 /*   By: ayhamdou <ayhamdou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 15:41:07 by ayhamdou          #+#    #+#             */
-/*   Updated: 2024/09/05 15:41:11 by ayhamdou         ###   ########.fr       */
+/*   Updated: 2024/09/24 11:51:21 by ayhamdou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../minishell.h"
+
+void	tokenizer(char *userInp)
+{
+	int		i;
+	char	**splittedArg;
+
+	i = 0;
+	splittedArg = ft_split(userInp, ' ');
+	while (splittedArg[i])
+	{
+		/* code */
+	}
+	
+	
+}
+
+void	parser(char *userInp)
+{
+	t_token *tokenList = NULL;
+	tokenizer(userInp);
+}
