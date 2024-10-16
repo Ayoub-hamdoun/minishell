ecd4f328c771daccfb239578202ffd833e2fe18d
ayoub <ayoub.hamdoun.g@gmail.com>
2024-10-16 16:35:17 +0100

    chore: Refactor code in the_export.c : norminette fixed

--- execution/builtins/the_export.c
+++ execution/builtins/the_export.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   the_export.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rallali <rallali@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ayhamdou <ayhamdou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:25:30 by rallali           #+#    #+#             */
-/*   Updated: 2024/10/16 15:47:03 by rallali          ###   ########.fr       */
+/*   Updated: 2024/10/16 16:33:55 by ayhamdou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ char	*extract_value(char *equal_pos)
 		return (strdup(equal_pos + 1));
 	return (NULL);
 }
-
 
 int	update_var(t_env *env, char *key, char *value, int append)
 {
@@ -102,9 +101,11 @@ int	update_var(t_env *env, char *key, char *value, int append)
 //     return 0;
 // }
 
-int check_append(char *cmd)
+int	check_append(char *cmd)
 {
-	int i = 0;
+	int	i;
+
+	i = 0;
 	while (cmd[i])
 	{
 		if (cmd[i] == '+' && cmd[i + 1] == '=')
@@ -113,50 +114,57 @@ int check_append(char *cmd)
 	}
 	return (1);
 }
-void add_new_var(t_env **env, char *key, char *value)
+
+void	add_new_var(t_env **env, char *key, char *value)
 {
-	t_env *new_var = malloc(sizeof(t_env));
-	if (!new_var) 
+	t_env	*new_var;
+	t_env	*last;
+
+	new_var = (t_env *) malloc(sizeof(t_env));
+	if (!new_var)
 	{
 		perror("Failed to allocate memory");
 		free(key);
 		free(value);
-		return;
+		return ;
 	}
 	new_var->key = key;
 	new_var->value = value;
 	new_var->next = NULL;
-
-	if (*env == NULL) 
+	if (*env == NULL)
 		*env = new_var;
-	else 
+	else
 	{
-		t_env *last = *env;
-		while (last->next) 
+		last = *env;
+		while (last->next)
 			last = last->next;
 		last->next = new_var;
 	}
 }
 
-void add_var(char *cmd, t_env **env,int append) 
+void	add_var(char *cmd, t_env **env, int append)
 {
-	char *equal_pos = strchr(cmd, '=');
-	char *key = extract_key(cmd, equal_pos);
-	char *value = extract_value(equal_pos);
-	if (!update_var(*env, key, value,append)) 
+	char	*equal_pos;
+	char	*key;
+	char	*value;
+
+	value = extract_value(equal_pos);
+	equal_pos = strchr(cmd, '=');
+	key = extract_key(cmd, equal_pos);
+	if (!update_var(*env, key, value, append))
 		add_new_var(env, key, value);
 }
 
-int check_equal(char *cmd) 
+int	check_equal(char *cmd)
 {
-	return strchr(cmd, '=') == NULL;
+	return (strchr(cmd, '=') == NULL);
 }
 
-void declare_x(t_env *env) 
+void	declare_x(t_env *env)
 {
-	while (env) 
+	while (env)
 	{
-		if (env->key && env->value) 
+		if (env->key && env->value)
 			printf("declare_x %s=\"%s\"\n", env->key, env->value);
 		else if (env->key && env->value == NULL)
 			printf("declare_x %s\n", env->key);
@@ -165,48 +173,53 @@ void declare_x(t_env *env)
 		env = env->next;
 	}
 }
-int check_valid(char *cmd)
-{
-	int i = 0;
-	int j = 0;
 
-	if ((cmd[0] >= 'A' && cmd[0] <= 'Z') || (cmd[0] >= 'a' && cmd[0] <= 'z') || cmd[0] == '_')
+int	check_valid(char *cmd)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if ((cmd[0] >= 'A' && cmd[0] <= 'Z')
+		|| (cmd[0] >= 'a' && cmd[0] <= 'z') || cmd[0] == '_')
 		j = 0;
 	else
-		return 1;
-
+		return (1);
 	while (cmd[i] != '=' && cmd[i])
 	{
-		if (!((cmd[i] >= 'A' && cmd[i] <= 'Z') || (cmd[i] >= 'a' && cmd[i] <= 'z') ||
-			  (cmd[i] >= '0' && cmd[i] <= '9') || cmd[i] == '_' || cmd[i] == '='))
+		if (!((cmd[i] >= 'A' && cmd[i] <= 'Z')
+				|| (cmd[i] >= 'a' && cmd[i] <= 'z')
+				|| (cmd[i] >= '0' && cmd[i] <= '9')
+				|| cmd[i] == '_' || cmd[i] == '='))
 		{
 			if (cmd[i] == '+' && cmd[i + 1] == '=')
 				i++;  // Skip '=' after '+'
 			else
-				return 1;
+				return (1);
 		}
 		i++;
 	}
-
-	return 0;
+	return (0);
 }
 
 void the_export(t_command *cmd, t_env **env)
 {
-	int i = 1;
+	int	i;
+	int	append;
 
-	if (!cmd->args[i]) 
+	i = 1;
+	if (!cmd->args[i])
 	{
 		declare_x(*env);
-		return;
+		return ;
 	}
-	
-	while (cmd->args[i]) 
+	while (cmd->args[i])
 	{
-		int append = check_append(cmd->args[i]);
+		append = check_append(cmd->args[i]);
 		if (check_valid(cmd->args[i]) == 0)
-			add_var(cmd->args[i], env, append); 
-		else 
+			add_var(cmd->args[i], env, append);
+		else
 			printf("export: `%s': not a valid identifier\n", cmd->args[i]);
 		i++;
 	}
